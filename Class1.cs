8eb9027a321871e8e6a65804de1bebb56558279d
Dallas <dallas.oliver91@gmail.com>
2019-08-29 13:37:32 -0400

    first commit

---
+++ Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    public class Dice
+    {
+        public int sides;
+        
+        public Dice(int number)
+        {
+            sides = number;
+
+        }
+
+        public int rollDie()
+        {
+            Random random = new Random();
+            return random.Next(1, sides);
+        }
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dice die = new Dice(20);
+            for (int i = 0; i < 100; i++)
+            {
+
+            Console.WriteLine(die.rollDie());
+            }
+           
+
+
+            Console.ReadLine();
+            
+        }
+
+    }
+}
